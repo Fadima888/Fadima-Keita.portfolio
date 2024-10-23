00bd32e8408fa982de3fbb4c69c414f7ddff58dd
Fadima <79483524+Fadima888@users.noreply.github.com>
2024-10-23 16:10:57 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Get the button
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    
+// Show the button when the user scrolls down 100px
+window.onscroll = function() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollToTopBtn.style.display = "flex";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+};
+
+// Scroll to the top when the button is clicked
+scrollToTopBtn.addEventListener("click", function() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
